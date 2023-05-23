0df120cb08e360ae1cd0d1ec5bac749fde78996b
Alen <alen.kimov@gmail.com>
2023-05-23 20:05:37 +0600

    fix: папка `input` не создавалась автоматически

--- bot/paths.py
+++ bot/paths.py
@@ -6,3 +6,5 @@ BASE_DIR = SCRIPT_DIR.parent
 INPUT_DIR = BASE_DIR / "input"
 PRIVATE_KEYS_TXT = INPUT_DIR / "private_keys.txt"
 TOKENS_TXT = INPUT_DIR / "tokens.txt"
+
+INPUT_DIR.mkdir(exist_ok=True)
