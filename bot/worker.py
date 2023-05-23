a9ff043cd008ff5826383af530a85f4025d58402
Alen <alen.kimov@gmail.com>
2023-05-23 21:02:28 +0600

    feat: выводы в консоль

--- bot/worker.py
+++ bot/worker.py
@@ -6,7 +6,7 @@ import aiohttp
 from eth_account import Account
 from eth_account.signers.local import LocalAccount
 
-from bot.pvu_api import get_auth_token, get_slots, get_user_info, harvest_plants
+from bot.pvu_api import get_auth_token, get_slots_by_location, get_land, get_user_info, harvest_plants
 from bot.pvu_api import water_plant, chase_crow
 from bot.pvu_api import buy_water, buy_scarecrow
 from bot.paths import PRIVATE_KEYS_TXT, TOKENS_TXT
@@ -40,6 +40,7 @@ async def work():
                 logger.error(f"Не удалось запросить токены авторизации")
             for token in tokens:
                 try:
+                    # Получаем данные о пользователе
                     user = await get_user_info(session, token)
                     logger.info(
                         f"[{user.public_address}]"
@@ -48,44 +49,88 @@ async def work():
                         f" scarecrows={user.chase_crow_tools}"
                     )
 
-                    slots = await get_slots(session, token)
-
+                    # По умолчанию обрабатываются только растения, принадлежащие пользователю
                     if PROCESS_ONLY_MY_PLANTS:
-                        slots = [slot for slot in slots if slot.owner_id == user.public_address]
-
-                    crow_amount = len(list(filter(lambda slot: slot.action_info.is_have_crow, slots)))
-                    need_water_amount = len(list(filter(lambda slot: slot.action_info.is_need_water, slots)))
-
-                    chase_crow_tools_to_buy = max(crow_amount - user.chase_crow_tools, 0)
-                    watering_tools_to_buy = max(need_water_amount - user.watering_tools, 0)
-
-                    if (user.le_amount - (chase_crow_tools_to_buy + watering_tools_to_buy) * 10) < 0:
-                        logger.warning(f"[{user.public_address}] Not enough LE to buy tools!")
+                        logger.info(
+                            f"[{user.public_address}]"
+                            f" Обработка растений, принадлежащих пользователю"
+                        )
                     else:
-                        if chase_crow_tools_to_buy > 0:
-                            await buy_scarecrow(session, token, chase_crow_tools_to_buy)
-                            logger.success(f"[{user.public_address}] Bought {chase_crow_tools_to_buy} chase crow tools!")
-                        if watering_tools_to_buy > 0:
-                            await buy_water(session, token, watering_tools_to_buy)
-                            logger.success(f"[{user.public_address}] Bought {watering_tools_to_buy} watering tools!")
-                        for slot in slots:
-                            if slot.action_info.is_have_crow:
-                                await chase_crow(session, token, slot.id)
+                        logger.info(
+                            f"[{user.public_address}]"
+                            f" Обработка всех растений, включая чужие"
+                        )
+
+                    # Получаем данные о землях пользователя
+                    lands = await get_land(session, token)
+                    for land in lands:
+                        # Получаем данные о слотах земли
+                        slots = await get_slots_by_location(session, token, land.location)
+
+                        # По умолчанию обрабатываются только растения, принадлежащие пользователю
+                        if PROCESS_ONLY_MY_PLANTS:
+                            slots = [slot for slot in slots if slot.owner_id == user.public_address]
+
+                        # Подсчет количество ворон и требующих полива растений
+                        crow_amount = len(list(filter(lambda slot: slot.action_info.is_have_crow, slots)))
+                        need_water_amount = len(list(filter(lambda slot: slot.action_info.is_need_water, slots)))
+                        logger.info(
+                            f"[{user.public_address}]"
+                            f" [land.x={land.location.x}, land.y={land.location.y}]"
+                            f" Ворон: {crow_amount}"
+                            f", требуют воды: {need_water_amount}"
+                        )
+
+                        # Подсчет количества инструментов к покупке
+                        chase_crow_tools_to_buy = max(crow_amount - user.chase_crow_tools, 0)
+                        watering_tools_to_buy = max(need_water_amount - user.watering_tools, 0)
+
+                        if (user.le_amount - (chase_crow_tools_to_buy + watering_tools_to_buy) * 10) < 0:
+                            logger.warning(f"[{user.public_address}] Not enough LE to buy tools!")
+                        else:
+                            if chase_crow_tools_to_buy > 0:
+                                await buy_scarecrow(session, token, chase_crow_tools_to_buy)
                                 logger.success(
-                                    f"[{user.public_address}] [{slot.location.x}, {slot.location.y}] chased!"
+                                    f"[{user.public_address}]"
+                                    f" Bought {chase_crow_tools_to_buy} chase crow tools!"
                                 )
-                            if slot.action_info.is_need_water:
-                                await water_plant(session, token, slot.id)
+                            if watering_tools_to_buy > 0:
+                                await buy_water(session, token, watering_tools_to_buy)
                                 logger.success(
-                                    f"[{user.public_address}] [{slot.location.x}, {slot.location.y}] watered!"
+                                    f"[{user.public_address}]"
+                                    f" Bought {watering_tools_to_buy} watering tools!"
                                 )
+                            for slot in slots:
+                                if slot.action_info.is_have_crow:
+                                    await chase_crow(session, token, slot.id)
+                                    logger.success(
+                                        f"[{user.public_address}]"
+                                        f" [land.x={land.location.x}, land.y={land.location.y}]"
+                                        f" [slot.x={slot.location.x}, slot.y={slot.location.y}]"
+                                        f" chased!"
+                                    )
+                                if slot.action_info.is_need_water:
+                                    await water_plant(session, token, slot.id)
+                                    logger.success(
+                                        f"[{user.public_address}]"
+                                        f" [land.x={land.location.x}, land.y={land.location.y}]"
+                                        f" [slot.x={slot.location.x}, slot.y={slot.location.y}]"
+                                        f" watered!"
+                                    )
                         now = datetime.utcnow().replace(tzinfo=timezone.utc)
                         slot_ids_to_harvest = []
+                        for slot in slots:
+                            if slot.harvest_time is not None and now > slot.harvest_time:
+                                slot_ids_to_harvest.append(slot.id)
                         if slot_ids_to_harvest:
-                            for slot in slots:
-                                if slot.harvest_time is not None and now > slot.harvest_time:
-                                    slot_ids_to_harvest.append(slot.id)
                             await harvest_plants(session, token, slot_ids_to_harvest)
+                            for slot in slots:
+                                logger.success(
+                                    f"[{user.public_address}]"
+                                    f" [land.x={land.location.x}, land.y={land.location.y}]"
+                                    f" [slot.x={slot.location.x}, slot.y={slot.location.y}]"
+                                    f" harvested!"
+                                )
                 except:
                     logger.error(f"[{token}] Ну удалось обработать аккаунт")
         logger.info(f"Sleep {DELAY} secs :)")
